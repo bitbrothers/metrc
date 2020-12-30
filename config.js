540d81090f7aae9098483fe517bf71b6052b4102
amoled27 <amodshinde1995@gmail.com>
2020-12-30 23:18:41 +0530

    expose environment configuration to external user

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
 config = {
-    licenseNo: "M10-0000004-LIC",
-    serverUrl: "https://sandbox-api-ca.metrc.com",
-    vendorKey: "y51gXYKY2zqeiWXFh8jAJOaDtOD1VfabCdWvlGpQ3EtGqBHd",
-    userKey: "FusVbe4Yv6W1DGNuxKNhByXU6RO6jSUPcbRCoRDD98VNXc4D",
-    authorization: "Basic eTUxZ1hZS1kyenFlaVdYRmg4akFKT2FEdE9EMVZmYWJDZFd2bEdwUTNFdEdxQkhkOkZ1c1ZiZTRZdjZXMURHTnV4S05oQnlYVTZSTzZqU1VQY2JSQ29SREQ5OFZOWGM0RA==",
-    lastModifiedEnd: "2020-08-18T06:30:00Z",
-    lastModifiedStart: "2020-08-18T06:30:00Z"
+    licenseNo: "",
+    serverUrl: "",
+    authorization: "",
+    lastModifiedEnd: "",
+    lastModifiedStart: ""
 }
 
-module.exports.config = config;
+module.exports = config;
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 
 const PORT = process.env.port || 5000;
-const transfers = require('./controllers/transfers.js');
+const transfers = require('./lib/controllers/transfers.js');
+const configParams = require('./lib/controllers/paramsConfig.js');
 
 const app = express();
 
 //code to test of APIs
 app.get('/', (req, res) => {
-    res.send(transfers.getIncomingTransfers());
+    configParams.setEnvironmentParams(
+        {
+            userKey: "FusVbe4Yv6W1DGNuxKNhByXU6RO6jSUPcbRCoRDD98VNXc4D",
+            vendorKey: "y51gXYKY2zqeiWXFh8jAJOaDtOD1VfabCdWvlGpQ3EtGqBHd",
+            serverUrl: "https://sandbox-api-ca.metrc.com",
+            licenseNo: "M10-0000004-LIC",
+            lastModifiedEnd: "2020-08-18T06:30:00Z",
+            lastModifiedStart: "2020-08-18T06:30:00Z"
+        }
+    )
+    setTimeout(() => {
+        res.send(transfers.getIncomingTransfers());
+    }, 200);
 });
 
 app.listen(PORT, () => {
