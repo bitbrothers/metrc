57f57cc16d557816165cc02716653798e9349a41
amoled27 <amodshinde1995@gmail.com>
2021-01-04 13:46:28 +0530

    get transaction by date range API integration

--- example/index.js
+++ example/index.js
@@ -10,10 +10,14 @@ const merchant = new metrc.Merchant({
 });
 
 
-merchant.getSalesTransactions(
+merchant.getSalesTransactionsByRange(
+  {
+    lastModifiedEnd: "2020-08-19",
+    lastModifiedStart: "2020-08-18",
+  },
   function response(data, error) {
-    if (error)
-      throw error
+      if(error)
+        throw error
     console.log(data);
   }
 );
